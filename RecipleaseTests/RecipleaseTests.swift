d31d03707775bc532919d795d3044876552f75bf
simonsbt <sabatiersimon1@gmail.com>
2024-01-08 11:38:33 +0100

    Added tests

--- RecipleaseTests/RecipleaseTests.swift
+++ RecipleaseTests/RecipleaseTests.swift
@@ -6,30 +6,67 @@
 //
 
 import XCTest
+import Alamofire
+@testable import Reciplease
+import SwiftData
 
+@MainActor
 final class RecipleaseTests: XCTestCase {
+    
+    func testAppStartsEmpty() throws {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Recipe.self, configurations: config)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let vm = ViewModel(modelContext: container.mainContext)
+
+        XCTAssertEqual(vm.favoriteRecipes.count, 0, "There should be 0 favorite recipes when the app is first launched.")
     }
+    
+    func testCreatingRecipe() throws {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Recipe.self, configurations: config)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let vm = ViewModel(modelContext: container.mainContext)
+        vm.recipes.append(Recipe(title: "Test recipe", imageUrl: "", ingredientFoodList: [], ingredientTextList: [], url: "", totalTime: 3600))
+        vm.addRecipeToFavorite(recipe: vm.recipes[0])
+
+        XCTAssertEqual(vm.favoriteRecipes.count, 1, "There should be 1 favorite recipe after adding sample data.")
     }
+    
+    func testDeletingRecipe() throws {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Recipe.self, configurations: config)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let vm = ViewModel(modelContext: container.mainContext)
+        vm.recipes.append(Recipe(title: "Test recipe", imageUrl: "", ingredientFoodList: [], ingredientTextList: [], url: "", totalTime: 3600))
+        vm.addRecipeToFavorite(recipe: vm.recipes[0])
+
+        vm.removeRecipeFromFavorite(recipe: vm.recipes[0])
+        XCTAssertEqual(vm.favoriteRecipes.count, 0, "There should be 0 favorite recipe after deleting data.")
     }
+    
+    func testAddingIngredients() throws {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Recipe.self, configurations: config)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        let vm = ViewModel(modelContext: container.mainContext)
+        vm.addIngredient(ingredient: "")
+        vm.addIngredient(ingredient: "@@@@")
+        vm.addIngredient(ingredient: "Chicken")
+
+        XCTAssertEqual(vm.ingredients.count, 1, "There should be 1 ingredient after adding sample data.")
     }
+    
+    func testClearingIngredients() throws {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Recipe.self, configurations: config)
 
+        let vm = ViewModel(modelContext: container.mainContext)
+        vm.addIngredient(ingredient: "Chicken")
+        vm.addIngredient(ingredient: "Strawberry")
+        vm.addIngredient(ingredient: "Apple")
+        vm.clearIngredients()
+
+        XCTAssertEqual(vm.ingredients.count, 0, "There should be 0 ingredient after clearing.")
+    }
 }
